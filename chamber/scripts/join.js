38574a45159bc2e861adf44c92c73425d8e85816
DerekJam <steeled@byui.edu>
2024-11-30 20:13:07 -0700

    updates

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,7 +1,61 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dialog = document.querySelector("dialog");
-    const openButton = document.querySelector("#openButton"); 
-    const closeButton = document.querySelector("#closeButton"); 
+    const openButton = document.querySelector("#openButtonNP"); 
+    const closeButton = document.querySelector("#closeButtonNP"); 
+
+  
+    openButton.addEventListener("click", () => {
+      dialog.showModal();
+    });
+  
+    closeButton.addEventListener("click", () => {
+      dialog.close();
+    });
+  });
+
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const dialog = document.querySelector("dialog");
+    const openButton = document.querySelector("#openButtonBr"); 
+    const closeButton = document.querySelector("#closeButtonBr"); 
+
+  
+    openButton.addEventListener("click", () => {
+      dialog.showModal();
+    });
+  
+    closeButton.addEventListener("click", () => {
+      dialog.close();
+    });
+  });
+
+
+
+
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const dialog = document.querySelector("dialog");
+    const openButton = document.querySelector("#openButtonSil"); 
+    const closeButton = document.querySelector("#closeButtonSil"); 
+
+  
+    openButton.addEventListener("click", () => {
+      dialog.showModal();
+    });
+  
+    closeButton.addEventListener("click", () => {
+      dialog.close();
+    });
+  });
+
+
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const dialog = document.querySelector("dialog");
+    const openButton = document.querySelector("#openButtonGold"); 
+    const closeButton = document.querySelector("#closeButtonGold"); 
+
   
     openButton.addEventListener("click", () => {
       dialog.showModal();
