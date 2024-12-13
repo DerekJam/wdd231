64b92365268ab230f09eb34000d93ef5cedb25b8
DerekJam <steeled@byui.edu>
2024-12-12 19:28:40 -0700

    Updates

---
+++ lakota/scripts/moreinfo.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dialog = document.querySelector("dialog");
+    const openButton = document.querySelector("#openButtonRC"); 
+    const closeButton = document.querySelector("#closeButtonRC"); 
+
+  
+    openButton.addEventListener("click", () => {
+      dialog.showModal();
+    });
+  
+    closeButton.addEventListener("click", () => {
+      dialog.close();
+    });
+  });
+
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const dialog = document.querySelector("dialog");
+    const openButton = document.querySelector("#openButtonBr"); 
+    const closeButton = document.querySelector("#closeButtonBr"); 
+
+  
+    openButton.addEventListener("click", () => {
+      dialog.showModal();
+    });
+  
+    closeButton.addEventListener("click", () => {
+      dialog.close();
+    });
+  });
+
+
+
+
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const dialog = document.querySelector("dialog");
+    const openButton = document.querySelector("#openButtonSil"); 
+    const closeButton = document.querySelector("#closeButtonSil"); 
+
+  
+    openButton.addEventListener("click", () => {
+      dialog.showModal();
+    });
+  
+    closeButton.addEventListener("click", () => {
+      dialog.close();
+    });
+  });
+
+
+  
