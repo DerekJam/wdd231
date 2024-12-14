b967031dead78f8483f13a299daded9950bd9669
DerekJam <steeled@byui.edu>
2024-12-13 22:33:36 -0700

    updates

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,7 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const dialog = document.querySelector("dialog");
-    const openButton = document.querySelector("#openButtonNP"); 
-    const closeButton = document.querySelector("#closeButtonNP"); 
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const dialog = document.querySelector("#dialog");
+    const openButton = document.querySelector("#openButton"); 
+    const closeButton = document.querySelector("#closeButton"); 
 
   
     openButton.addEventListener("click", () => {
@@ -16,52 +17,85 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   document.addEventListener('DOMContentLoaded', () => {
-    const dialog = document.querySelector("dialog");
-    const openButton = document.querySelector("#openButtonBr"); 
-    const closeButton = document.querySelector("#closeButtonBr"); 
+    const dialog1 = document.querySelector("#dialog1");
+    const openButton1 = document.querySelector("#openButton1"); 
+    const closeButton1 = document.querySelector("#closeButton1"); 
 
   
-    openButton.addEventListener("click", () => {
-      dialog.showModal();
+    openButton1.addEventListener("click", () => {
+      dialog1.showModal();
     });
   
-    closeButton.addEventListener("click", () => {
-      dialog.close();
+    closeButton1.addEventListener("click", () => {
+      dialog1.close();
+    });
+  });
+
+
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const dialog2 = document.querySelector("#dialog2");
+    const openButton2 = document.querySelector("#openButton2"); 
+    const closeButton2 = document.querySelector("#closeButton2"); 
+
+  
+    openButton2.addEventListener("click", () => {
+      dialog2.showModal();
+    });
+  
+    closeButton2.addEventListener("click", () => {
+      dialog2.close();
     });
   });
 
 
 
-
-  
   document.addEventListener('DOMContentLoaded', () => {
-    const dialog = document.querySelector("dialog");
-    const openButton = document.querySelector("#openButtonSil"); 
-    const closeButton = document.querySelector("#closeButtonSil"); 
+    const dialog3 = document.querySelector("#dialog3");
+    const openButton3 = document.querySelector("#openButton3"); 
+    const closeButton3 = document.querySelector("#closeButton3"); 
 
   
-    openButton.addEventListener("click", () => {
-      dialog.showModal();
+    openButton3.addEventListener("click", () => {
+      dialog3.showModal();
     });
   
-    closeButton.addEventListener("click", () => {
-      dialog.close();
+    closeButton3.addEventListener("click", () => {
+      dialog3.close();
     });
   });
 
 
-  
+
   document.addEventListener('DOMContentLoaded', () => {
-    const dialog = document.querySelector("dialog");
-    const openButton = document.querySelector("#openButtonGold"); 
-    const closeButton = document.querySelector("#closeButtonGold"); 
+    const dialog4 = document.querySelector("#dialog4");
+    const openButton4 = document.querySelector("#openButton4"); 
+    const closeButton4 = document.querySelector("#closeButton4"); 
 
   
-    openButton.addEventListener("click", () => {
-      dialog.showModal();
+    openButton4.addEventListener("click", () => {
+      dialog4.showModal();
     });
   
-    closeButton.addEventListener("click", () => {
-      dialog.close();
+    closeButton4.addEventListener("click", () => {
+      dialog4.close();
+    });
+  });
+
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const dialog5 = document.querySelector("#dialog5");
+    const openButton5 = document.querySelector("#openButton5"); 
+    const closeButton5 = document.querySelector("#closeButton5"); 
+
+  
+    openButton5.addEventListener("click", () => {
+      dialog5.showModal();
+    });
+  
+    closeButton5.addEventListener("click", () => {
+      dialog5.close();
     });
   });
