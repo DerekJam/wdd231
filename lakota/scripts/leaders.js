c3a3ff45ab440ad1e4e3ff4fb7c18d89cd15045e
DerekJam <steeled@byui.edu>
2024-12-14 23:25:39 -0700

    updates

--- lakota/scripts/leaders.js
+++ lakota/scripts/leaders.js
@@ -3,26 +3,23 @@
 fetch('scripts/leaders.json')
   .then(response => response.json())
   .then(data => {
-    const companyListSection = document.getElementById('leaders');
-    const desiredStatuses = ["Gold", "Silver"];
+    const companyListSection = document.getElementById('company-list');
 
-
-      data.leaders.forEach((leader, index) => {
-        if (desiredStatuses.includes(leader['years-lived'])) {
+      data.companies.forEach((company, index) => {
         const companySection = document.createElement('section');
-        companySection.classList.add('gridlines1');
+        companySection.classList.add('gridlines4');
 
         companySection.innerHTML = `
           <img src="images/tcclogo2.png" alt="TCC logo" width="30" height="20"></img><br>
-<p><b>${leader.name}</b></p>
-<p>Birth: ${leader.birth}</p>
-<p>Death: ${leader.death}</p>
-<p>Info: ${leader.info}</p>
-<p>Lifespan: ${leader.lifespan}</p>
+<p><b>${company.name}</b></p>
+<p>Phone: ${company.phone}</p>
+<p>Website: <a href="${company.website}" target="_blank">${company.website}</a></p>
+<p>Contact: ${company.contact}</p>
+<p>Status: <b>${company.status}</b></p>
 `;
 
 companyListSection.appendChild(companySection);
-        }
       });
       })
       .catch(error => console.error('Error loading member data:', error));
+   
