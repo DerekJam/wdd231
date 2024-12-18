e123aa17e85525d4dd082819e949f14a33eb3f95
DerekJam <steeled@byui.edu>
2024-12-17 19:28:54 -0700

    updates

---
+++ chamber2/scripts/leaders.js
@@ -0,0 +1,24 @@
+
+
+fetch('scripts/leaders.json')
+  .then(response => response.json())
+  .then(data => {
+    const companyListSection = document.getElementById('company-list');
+
+      data.companies.forEach((company, index) => {
+        const companySection = document.createElement('section');
+        companySection.classList.add('gridlines4');
+
+        companySection.innerHTML = `
+<p><b>${company.name}</b></p>
+<p>Contact: ${company.contact}</p>
+<p>Phone: ${company.phone}</p>
+<p><a href="${company.website}" target="_blank">Website</a></p>
+<p>Status: <b>${company.status}</b></p>
+`;
+
+companyListSection.appendChild(companySection);
+      });
+      })
+      .catch(error => console.error('Error loading member data:', error));
+   
